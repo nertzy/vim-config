ed6c11867e0db4c8378e329fb84403e7418e3af2
Grant Hutchins <grant.hutchins@strongholdim.com>
2024-02-08 11:10:25 -0600

    Configure null-ls for nvim

---
+++ after/null-ls.lua
@@ -0,0 +1,6 @@
+require("null-ls").setup({
+  sources = {
+    require("null-ls").builtins.formatting.shfmt, -- shell script formatting
+    require("null-ls").builtins.formatting.prettier, -- markdown formatting
+  },
+})
